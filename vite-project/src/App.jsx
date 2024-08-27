e5ba22d872fc478166b2cd2febd6c0a9d6feb2fa
JoaoVitorCarottaRibeiro <joaovitcr@gmail.com>
2024-08-27 14:43:39 -0300

    teste

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './Components/Header/Header'
+import Home from './Components/HomeLP/Home'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+
+  )
+}
+
+export default App
---
+++ vite-project/src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import './header.css'
+
+export default function Header() {
+   return (
+    <nav>
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Sobre</a>
+            </li>
+            <li>
+                <a href="#">Contato</a>
+            </li>
+        </ul>
+    </nav>
+   ) 
+}
